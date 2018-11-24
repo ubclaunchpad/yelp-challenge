44f71df4266bd28d3dfb84973851cc5b883a6107
Sarina Chen <sarina.chen96@gmail.com>
2018-11-24 11:59:35 -0800

    add preprocessing

--- clustering/preprocess.py
+++ clustering/preprocess.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 import pandas as pd
+import operator
 
 BUSINESS_DATA = "data/yelp_academic_dataset_business.json"
 CHECKIN_DATA = "data/yelp_academic_dataset_checkin.json"
@@ -23,14 +24,97 @@ def filter_restaurants(businesses, max_restaurants=None):
     return businesses
 
 
-def create_feature_matrix(restaurants):
+def create_feature_matrix(res):
     """
     Returns a NumPy array that contains all of the features for
     restaurants that will be fed into the clustering model.
     """
+    # Finds all unique restaurant categories
+    categories = {}
 
-    # TODO: form a valid NumPy array from the restaurant data
-    return np.identity(1)
+    for index, row in res.iterrows():
+        for category in row['categories'].split(', '):
+            if categories.get(category) == None:
+                categories[category] = 1
+            else:
+                categories[category] += 1
+
+    sortedCat = sorted(categories.items(), key=operator.itemgetter(1), reverse=True)
+
+    for index, cat in enumerate(sortedCat):
+        if cat[1] < 8:
+            break
+
+        res[cat[0]] = res.apply(lambda row: (1 if cat[0] in row['categories'] else 0), axis=1)
+
+    # manually selected attributes that are relevant to restaurants and have a significant response rating according to kaggle
+    # 'BusinessParking': "{'garage': False, 'street': True, 'validated': False, 'lot': False, 'valet': False}"
+
+    attributes = [
+        'BusinessAcceptsCreditCards',
+        'GoodForKids',
+        'HasTV',
+        'BikeParking',
+        'WheelchairAccessible',
+        'RestaurantsReservations',
+        'HappyHour',
+        'Caters',
+        'DogsAllowed',
+        'OutdoorSeating',
+        'RestaurantsTakeOut',
+        'RestaurantsDelivery'
+    ]
+
+    nonBoolAttributes = {
+                        'Alcohol': ['none', 'beer_and_wine', 'full_bar'],
+                        'RestaurantsPriceRange2':['2', '1', '3', '4'],
+                        'RestaurantsAttire': ['casual', 'dressy', 'formal'],
+                        'BusinessParking': ['garage', 'street', 'validated', 'lot', 'valet']
+                        }
+
+    def addParkingColumns(row, parkingType):
+        if row['attributes'] is not None and 'BusinessParking' in row['attributes'] and valType in row['attributes']['BusinessParking']:
+            parking = json.loads(row['attributes']['BusinessParking'].replace('\'', '"').replace('False', '"False"').replace('True', '"True"'))
+            if parking[parkingType] == 'True':
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+
+    for att in attributes:
+        res[att] = res.apply(lambda row: 1 if row['attributes'] is not None
+                                          and att in row['attributes']
+                                          and row['attributes'][att] == 'True'
+                                          else 0, axis=1)
+
+    # add non-boolean attributes
+    for att in nonBoolAttributes:
+        for valType in nonBoolAttributes[att]:
+            if valType is 'none':
+                continue
+            key = att + '_' + valType;
+
+            if att == 'BusinessParking':
+                res[key] = res.apply(lambda row: addParkingColumns(row, valType), axis=1)
+            else:
+                res[key] = res.apply(lambda row:
+                                              (1 if row['attributes'] is not None
+                                               and att in row['attributes']
+                                               and row['attributes'][att] == valType
+                                               else 0), axis=1)
+    # delete unnecessary columns
+    del res['attributes']
+    del res['categories']
+    del res['hours']
+    del res['name']
+    del res['address']
+    del res['city']
+    del res['neighborhood']
+    del res['postal_code']
+    del res['state']
+
+    return res.values
 
 
 def preprocess(debug=False):
